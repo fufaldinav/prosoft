9f5a21aa063b0577be7cfe9f17cdfc4ce885014a
Artem <artem.fufaldin@gmail.com>
2020-04-02 08:19:43 +0500

    Добавил страницы для 404 и ошибки получения данных

---
+++ src/components/AppNotFound.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="d-flex flex-row align-items-center h-100">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-12 text-center">
+                    <span class="display-1 d-block">404</span>
+                    <div class="mb-4 lead">The page you are looking for was not found.</div>
+                    <router-link to="/">Back to Home</router-link>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'AppNotFound',
+    };
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import AppBdError from '../components/AppBdError';
+import AppNotFound from '../components/AppNotFound';
 import AppTable from '../components/AppTable';
 
 Vue.use(Router);
@@ -8,6 +10,8 @@ const router = new Router({
     mode: 'history',
     routes: [
         { path: '/', name: 'table', component: AppTable },
+        { path: '/error', name: 'error', component: AppBdError },
+        { path: '*', name: '404', component: AppNotFound },
     ],
 });
 
