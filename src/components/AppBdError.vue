5cb1262317168ddc43b3205f662c89f5a53a137c
Artem <artem.fufaldin@gmail.com>
2020-04-08 23:53:37 +0500

    Возможность вернуться назад

--- src/components/AppBdError.vue
+++ src/components/AppBdError.vue
@@ -4,8 +4,15 @@
             <div class="row justify-content-center">
                 <div class="col-md-12 text-center">
                     <span class="display-1 d-block">Error</span>
-                    <div class="mb-4 lead">Something was wrong. Try to reset filters or check your link.</div>
-                    <router-link to="/">Reset filters and go back</router-link>
+                    <div class="mb-4 lead">Something was wrong.</div>
+                    <template v-if="fromPath !== null">
+                        <router-link :to="fromPath">Go back</router-link>
+                        or
+                        <router-link to="/">reset filters</router-link>
+                    </template>
+                    <template v-else>
+                        <router-link to="/">Go to Home</router-link>
+                    </template>
                 </div>
             </div>
         </div>
@@ -16,9 +23,17 @@
     export default {
         name: 'AppBdError',
 
+        data() {
+            return {
+                fromPath: null,
+            };
+        },
+
         beforeRouteEnter(to, from, next) {
             if (from.name !== 'table') {
                 next('/');
+            } else if (typeof from.query.page === 'number') { //TODO найти "павильный" способ вычислять переход по внешним ссылкам
+                next(vm => vm.fromPath = from.fullPath);
             } else {
                 next();
             }
